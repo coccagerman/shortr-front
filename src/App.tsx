fc9750163a309e77394578d7b09797cf4c0d5673
German Cocca <coccagerman@gmail.com>
2022-03-21 22:10:33 -0300

    Creating remote repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import './App.css'
+import styled from 'styled-components'
+
+import logo from './assets/logo.png'
+
+const App:React.FC = () => {
+
+  const [urlToShorten, setUrlToShorten] = useState<string>('')
+  const [shortenedUrl, setShortenedUrl] = useState<string | null>(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='App'>
+
+      <Header>
+        <img src={logo} alt='Logo' />
+      </Header>
+
+      <div className='input-container'>
+        <label htmlFor='url'>Enter the URL you'd like to shorten:</label>
+        <input type='text' onChange={e => setUrlToShorten(e.target.value)}/>
+      </div>
+
+      <button disabled={urlToShorten.length > 0}>Shorten URL</button>
+
+      {shortenedUrl && <p>Your shortened URL is: <a href={shortenedUrl} target='_blank' rel='noreferrer' >{shortenedUrl}</a></p>}
+    </section>
+  )
 }
 
-export default App;
+  // Create a Title component that'll render an <h1> tag with some styles
+  const Header = styled.header`
+  display: flex,
+  allign-items: center,
+  background-color: red,
+  width: 100%
+  `;
+  
+export default App
